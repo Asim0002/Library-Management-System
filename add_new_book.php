d55c1d587777cc2c190d16607a0a427dca0399fe
Asim0002 <143107823+Asim0002@users.noreply.github.com>
2025-05-30 01:26:07 +0530

    Update add_new_book.php

--- add_new_book.php
+++ add_new_book.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['admin_id'])) {
   header("Location: login.php");
   exit();
 }
-include 'db.php'; // Make sure db.php uses PDO
+include 'db.php'; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $title = trim($_POST['title']);
